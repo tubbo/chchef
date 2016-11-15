f86a54f5f1e2ed56789bf8555c61d522f0ac04cd
Tom Scott <tscott@weblinc.com>
2016-11-15 00:05:07 -0500

    Fix shellcheck warnings

--- share/chchef/chchef.sh
+++ share/chchef/chchef.sh
@@ -8,27 +8,27 @@ chchef() {
   case "$1" in
   init)
     name=$2
-    mkdir -p ~/.chef/$name
-    mv ~/.chef/knife.rb ~/.chef/$name/knife.rb
-    mv ~/.chef/${USER}.pem ~/.chef/$name/${USER}.pem
-    if [[ -f ~/.chef/$name-validator.pem ]]; then
-      mv ~/.chef/$name-validator.pem ~/.chef/$name/$name-validator.pem
+    mkdir -p "$HOME/.chef/$name"
+    mv "$HOME/.chef/knife.rb" "$HOME/.chef/$name/knife.rb"
+    mv "$HOME/.chef/${USER}.pem" "$HOME/.chef/$name/${USER}.pem"
+    if [[ -f $HOME/.chef/$name-validator.pem ]]; then
+      mv "$HOME/.chef/$name-validator.pem" "$HOME/.chef/$name/$name-validator.pem"
     fi
-    chchef $name
+    chchef "$name"
     ;;
   *)
     name=$1
 
-    if [[ -d ~/.chef/$1 ]]; then
-      rm -f ~/.chef/knife.rb ~/.chef/${USER}.pem ~/.chef/${name}-validator.pem
-      ln -s ~/.chef/$name/knife.rb ~/.chef/knife.rb
-      ln -s ~/.chef/$name/${USER}.pem ~/.chef/${USER}.pem
+    if [[ -d $HOME/.chef/$1 ]]; then
+      rm -f "$HOME/.chef/knife.rb" "$HOME/.chef/${USER}.pem" "$HOME/.chef/${name}-validator.pem"
+      ln -s "$HOME/.chef/$name/knife.rb" "$HOME/.chef/knife.rb"
+      ln -s "$HOME/.chef/$name/${USER}.pem" "$HOME/.chef/${USER}.pem"
 
-      if [[ -f ~/.chef/$name/$name-validator.pem ]]; then
-        ln -s ~/.chef/$name/$name-validator.pem ~/.chef/$name-validator.pem
+      if [[ -f "$HOME/.chef/$name/$name-validator.pem" ]]; then
+        ln -s "$HOME/.chef/$name/$name-validator.pem" "$HOME/.chef/$name-validator.pem"
       fi
     else
-      echo "No such file or directory: ~/.chef/$1"
+      echo "No such file or directory: $HOME/.chef/$1"
       return
     fi
 
